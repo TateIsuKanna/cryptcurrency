06a1970edb9091e540bfe4ea58fa9ebd052fde9c
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2018-02-16 20:21:45 +0900

    整理

--- cryptcurrency.py
+++ cryptcurrency.py
@@ -1,6 +1,5 @@
 import hashlib
 import OpenSSL
-import datetime
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
@@ -14,11 +13,9 @@ class Transaction():
         self.recipient=recipient
         self.amount=amount
         
-
-        p=datetime.datetime.now()
         self.signature=OpenSSL.crypto.sign(OpenSSL.crypto.PKey.from_cryptography_key(private_key),"from"+str(self.sender)+" to"+str(self.recipient)+" "+str(self.amount),"sha256")
-    def __repr__(self):
-        return "from"+str(self.sender)+" to"+str(self.recipient)+" "+str(self.amount)+str(self.signature)
+    def __str__(self):
+        return str(self.sender)+" -> "+str(self.recipient)+" "+str(self.amount)+" "+str(self.signature)[:10]+"..."
 
 class Block():
     def __init__(self):
@@ -29,17 +26,7 @@ class Block():
         self.transactions.append(transaction)
     def calc_hash(self):
         return hashlib.sha256((str(self.transactions)+str(self.proof)+str(self.previous_hash)).encode()).hexdigest()
-    def work(self,ledger=None):
-        if ledger:
-            self.previous_hash=ledger.blocks[-1].calc_hash()
-        while True:
-            temp=self.calc_hash()
-            if temp.startswith("000"):
-                print(temp)
-                break
-            self.proof=temp
-
-    def __repr__(self):
+    def __str__(self):
         return "previous_hash:"+str(self.previous_hash)+"\n"+"proof:"+str(self.proof)+"\n"+str("\n".join([str(t) for t in self.transactions]))
 
 class Blockchain():
@@ -47,9 +34,20 @@ class Blockchain():
         self.blocks=[]
 
     def add_block(self,block):
+        if len(self.blocks)>0:
+            block.previous_hash=self.blocks[-1].calc_hash()
         self.blocks.append(block)
+        self.work()
 
-    def __repr__(self):
+    def work(self):
+        while True:
+            temp=self.blocks[-1].calc_hash()
+            if temp.startswith("0000"):
+                print(temp)
+                break
+            self.blocks[-1].proof=temp
+
+    def __str__(self):
         return str("\n\n".join([str(t) for t in self.blocks]))
 
     #@staticmethod
@@ -63,21 +61,14 @@ class Blockchain():
 ledger=Blockchain()
 b=Block()
 b.add_transaction(Transaction(0,0xc248712dff34,5000000000000000))
-b.work()
 ledger.add_block(b)
+print(b)
+
+print()
 
 b=Block()
 b.add_transaction(Transaction(0,0x34fff234324,50))
 b.add_transaction(Transaction(0xc248712dff34,0x34fff234324,5000000000000000))
 b.add_transaction(Transaction(0x34124,0x38748,10))
-b.work(ledger)
 ledger.add_block(b)
-
-import random
-for i in range(100):
-    b=Block()
-    for j in range(random.randint(1,10)):
-        b.add_transaction(Transaction(random.randint(0,10000000),random.randint(0,10000000),random.randint(0,38491834341)))
-    b.work(ledger)
-    print(b)
-    ledger.add_block(b)
+print(b)
