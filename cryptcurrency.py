371c13f1b83c95e46d0025d13e1a2fac00001b07
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2018-02-26 00:35:41 +0900

    途中．アドレスとか

--- cryptcurrency.py
+++ cryptcurrency.py
@@ -4,8 +4,12 @@ import base64
 import cryptography
 import struct
 
-with open("private.pem", "rb") as key_file:
-    private_key=OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM,key_file.read())
+class CryptcurrencyUtil():
+    @classmethod
+    def publickey_to_address(self,pub):
+        digest=hashlib.sha256(pub).digest()
+        return base64.b64encode(digest+hashlib.sha256(digest).digest()[:4]).decode()
+
 class Transaction():
     def __init__(self,sender,recipient,amount):
         #TODO:不正なアドレス
@@ -21,15 +25,15 @@ class Transaction():
 
 class Block():
     def __init__(self):
+        self.previous_hash=b""
         self.transactions=[]
         self.nonce=b""
-        self.previous_hash=b""
     def add_transaction(self,transaction):
         #TODO:署名確認
         #TODO:残高確認
         self.transactions.append(transaction)
     def calc_hash(self):
-        return hashlib.sha256((str(self.transactions)+str(self.nonce)+str(self.previous_hash)).encode()).hexdigest()
+        return hashlib.sha256((str(self.previous_hash)+str(self.transactions)+str(self.nonce)).encode()).hexdigest()
     def __str__(self):
         return "previous_hash "+str(self.previous_hash)+"\n"+"nonce "+str(self.nonce)+"\n"+str("\n".join([str(t) for t in self.transactions]))
 
@@ -45,22 +49,28 @@ class Blockchain():
 
     def work(self):
         while True:
-            temp=self.blocks[-1].calc_hash()
-            if temp.startswith("0000"):
-                print(temp)
+            temp_nonce=self.blocks[-1].calc_hash()
+            if temp_nonce.startswith("0000"):
+                print(temp_nonce)
                 break
-            self.blocks[-1].nonce=temp
+            self.blocks[-1].nonce=temp_nonce
 
     def __str__(self):
         return str("\n\n".join([str(t) for t in self.blocks]))
 
 ledger=Blockchain()
 
-b=Block()
+with open("private.pem", "rb") as key_file:
+    private_key=OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM,key_file.read())
 s=private_key.to_cryptography_key().public_key().public_bytes(cryptography.hazmat.primitives.serialization.Encoding.DER,cryptography.hazmat.primitives.serialization.PublicFormat.SubjectPublicKeyInfo)
 m=base64.b64decode("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2zSJb6e9tJKfKSNKDOWL zxa/9ZOfquIXuZA/4QGvuvasT7km8IamyTS3Ft0jXjCaGstiFy2jKcLv6rumiC4A bkkwPGpBT0bnhpDfnGXRlL2qvK4sgZkNc/3DFjbbIFzZy3TaCt41+KpRY7aP9Epp C+kVB9YQ+lg5CcUAqFL2i2i9PbonE/3W5p5f0C/ewiiPWfEoSD6zDP2ZRKPMpVbF NQL2iw1LnYeZKEp9SWwA+h/VTt3p0do1Z5hfNe6WdbrdHsl9cBzP5KjfecqqORSZ gZAF13ubLtsNEsJXop375CcJ4O8n0ub0KJaPZVKdiqayrDd1gC+CXekKNxYZfMDp yQIDAQAB")
+
+print(CryptcurrencyUtil.publickey_to_address(m))
+
 t=Transaction(s,m,5000000000000000)
 t.sign(private_key)
+
+b=Block()
 b.add_transaction(t)
 ledger.add_block(b)
 
